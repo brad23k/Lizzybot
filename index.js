7b7a2d9bbc2f34fd4d809e186a1c2765b1f5002a
brad23k <bradleykris102@gmail.com>
2025-07-17 18:25:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('LizzyBot is running...');
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Basic reply
+bot.start((ctx) => ctx.reply('Hey, I’m LizzyBot. How can I please you today? 😘'));
+bot.help((ctx) => ctx.reply('Send me a message or a tip, baby 😏'));
+
+// Echo user messages
+bot.on('text', (ctx) => {
+  ctx.reply(`You said: ${ctx.message.text}`);
+});
+
+// Launch the bot
+bot.launch().then(() => {
+  console.log('LizzyBot is live 🚀');
+});
+
+// Prevent Render from shutting down
+setInterval(() => {
+  console.log('Ping to keep alive...');
+}, 60000);
